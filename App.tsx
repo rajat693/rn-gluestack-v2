6b77788c19dcdf72b5e9f8e566e76444f12d99d9
ankit-tailor <tailorankit3345@gmail.com>
2024-07-29 16:02:15 +0530

    fix: import aliasing

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import {SafeAreaView, Text} from 'react-native';
-import {GluestackUIProvider} from './components/ui/gluestack-ui-provider';
+import {GluestackUIProvider} from '@/components/ui/gluestack-ui-provider';
 import './global.css';
+import React from 'react';
 
 function App() {
   return (
     <GluestackUIProvider>
       <SafeAreaView className="flex-1 items-center justify-center bg-error-50">
-        <Text className="text-success-500">hello world</Text>
+        <Text className="text-success-500 text-xl">hello world</Text>
       </SafeAreaView>
     </GluestackUIProvider>
   );
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './',
+        },
+      },
+    ],
+  ],
 };
